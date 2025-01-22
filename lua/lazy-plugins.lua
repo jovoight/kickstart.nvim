fb8773610201524e5870242feaba8d50974dfa81
Voight, Josiah <josiah.voight@abbvie.com>
2025-01-22 12:13:54 -0600

    add trouble plugin

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -28,40 +28,22 @@ require('lazy').setup({
   -- include a plugin definition from file lua/path/name.lua
 
   require 'plugins/gitsigns',
-
   require 'plugins/which-key',
-
   require 'plugins/telescope',
-
   require 'plugins/lspconfig',
-
   require 'plugins/conform',
-
   require 'plugins/cmp',
-
   require 'plugins/kanagawa_paper',
-
   require 'plugins/todo-comments',
-
   require 'plugins/mini',
-
   require 'plugins/treesitter',
-
-  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
-
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
   require 'plugins.debug',
   require 'plugins.indent_line',
   require 'plugins.lint',
   require 'plugins.autopairs',
   require 'plugins.neo-tree',
   require 'plugins.null_ls',
+  require 'plugins.trouble',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
