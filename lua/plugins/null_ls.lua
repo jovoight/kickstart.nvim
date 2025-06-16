676a3eac22d7e780e83d844e871ac3566c23df12
Voight, Josiah <josiah.voight@abbvie.com>
2025-06-16 10:31:11 -0500

    improve handling for auto formatting

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -2,6 +2,7 @@ return {
   'nvimtools/none-ls.nvim',
   config = function()
     local null_ls = require 'null-ls'
+    local formatting_augroup = vim.api.nvim_create_augroup('LspFormatting', {})
     null_ls.setup {
       debug = true,
       sources = {
@@ -9,13 +10,24 @@ return {
         null_ls.builtins.formatting.clang_format,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.prettier,
       },
+      on_attach = function(client, bufnr)
+        if client.supports_method 'textDocument/formatting' then
+          vim.api.nvim_clear_autocmds {
+            group = formatting_augroup,
+            buffer = bufnr,
+          }
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            group = formatting_augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format { bufnr = bufnr }
+            end,
+          })
+        end
+      end,
     }
     vim.keymap.set('n', '<leader>fo', vim.lsp.buf.format, {})
-    -- Auto-format cpp files on save
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      pattern = { '*.cpp', '*.h' },
-      command = 'lua vim.lsp.buf.format()',
-    })
   end,
 }
